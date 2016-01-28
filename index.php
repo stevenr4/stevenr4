cd1d92ec5c2419593edbdd12c5fbf1f433be27ae
stevenr4 <stevenr41@hotmail.com>
2016-01-28 15:25:58 -0800

    Initial commit: Adding index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!doctype html>
+
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <title>Steven Rogers</title>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+</head>
+<style>
+</style>
+<body>
+  <h1>Starting Portfolio</h1>
+</body>
+</html>
