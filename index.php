c44da033a5b30cf6e8cefa52e50f4def1e5b0c0c
stevenr4 <stevenr41@hotmail.com>
2016-02-02 16:33:52 -0800

    Added more stuff, still WIP. Testing captcha

--- index.php
+++ index.php
@@ -5,11 +5,123 @@
   <meta charset="utf-8">
   <title>Steven Rogers</title>
   <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
-  <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
-  <style>
-  </style>
+  <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" />
+  <link rel="stylesheet" type="text/css" href="style.css" />
+  <!-- reCaptcha -->
+  <script src='https://www.google.com/recaptcha/api.js'></script>
+  <!-- end reCaptcha -->
+  <!-- Google Fonts -->
+  <script type="text/javascript">
+  WebFontConfig = {
+    google: { families: [ 'Lato:400,300,700:latin' ] }
+  };
+  (function() {
+    var wf = document.createElement('script');
+    wf.src = ('https:' == document.location.protocol ? 'https' : 'http') +
+      '://ajax.googleapis.com/ajax/libs/webfont/1/webfont.js';
+    wf.type = 'text/javascript';
+    wf.async = 'true';
+    var s = document.getElementsByTagName('script')[0];
+    s.parentNode.insertBefore(wf, s);
+  })(); </script>
+  <!-- end Google Fonts -->
 </head>
 <body>
-  <h1>Starting Portfolio</h1>
+  <div class="container">
+    <div class="page">
+      <div class="row">
+        <div class="col-xs-12 col-sm-9">
+          <h1>Steven Rogers</h1>
+          <h2>Full Stack Web Developer</h2>
+        </div>
+        <div class="col-xs-12 col-sm-9">
+        </div>
+      </div>
+      <div class="segment">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4">
+            <h3>About Steven</h3>
+          </div>
+          <div class="col-xs-12 col-sm-8">
+            <p>
+              I am an enthusiastic, passionate full-stack web developer of over two years based in
+              Bellevue, WA. I have a passion for discovery, tinkering, and making things. Currently, I am looking for a
+              position where I can develop both myself and a great product.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="segment">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4">
+            <h3>Skills</h3>
+          </div>
+          <div class="col-xs-12 col-sm-8">
+            <p>
+              I am an enthusiastic, passionate full-stack web developer of over two years based in
+              Bellevue, WA. I have a passion for discovery, tinkering, and making things. Currently, I am looking for a
+              position where I can develop both myself and a great product.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="segment">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4">
+            <h3>Experience</h3>
+          </div>
+          <div class="col-xs-12 col-sm-8">
+            <p>
+              I am an enthusiastic, passionate full-stack web developer of over two years based in
+              Bellevue, WA. I have a passion for discovery, tinkering, and making things. Currently, I am looking for a
+              position where I can develop both myself and a great product.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="segment">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4">
+            <h3>Portfolio</h3>
+          </div>
+          <div class="col-xs-12 col-sm-8">
+            <p>
+              I am an enthusiastic, passionate full-stack web developer of over two years based in
+              Bellevue, WA. I have a passion for discovery, tinkering, and making things. Currently, I am looking for a
+              position where I can develop both myself and a great product.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <div class="segment">
+        <div class="row">
+          <div class="col-xs-12 col-sm-4">
+            <h3>More About Steven</h3>
+          </div>
+          <div class="col-xs-12 col-sm-8">
+            <p>
+              I am an enthusiastic, passionate full-stack web developer of over two years based in
+              Bellevue, WA. I have a passion for discovery, tinkering, and making things. Currently, I am looking for a
+              position where I can develop both myself and a great product.
+            </p>
+          </div>
+        </div>
+      </div>
+
+      <form>
+        <input name="email" type="email" placeholder="youremail@domain.com" class="form-control" />
+        <input name="subject" type="text" placeholder="Subject of the Email" class="form-control" />
+        <input name="phone" type="tel" placeholder="Phone (optional)" class="form-control" />
+        <input name="name" type="text" placeholder="Name (optional)" class="form-control" />
+        <textarea name="content" class="form-control"></textarea>
+        <div class="g-recaptcha" data-sitekey="6LfrNRcTAAAAAGyHnfc-S0JiToIy5jZvv3xsX_2s"></div>
+      </form>
+
+    </div>
+  </div>
 </body>
 </html>
