5cad542a00a5c2a223b65327548518a0f005dec3
stevenr4 <stevenr41@hotmail.com>
2016-02-04 02:13:36 -0800

    SMTP doesn't work on heroku, switching to google

--- email.php
+++ email.php
@@ -3,39 +3,53 @@
   if ((isset($_REQUEST["email"]) && !empty($_REQUEST["email"])) &&
       (isset($_REQUEST["message"]) && !empty($_REQUEST["message"]))) {
 
-      // the message
-      $msg = $_REQUEST['message'];
-      // use wordwrap() if lines are longer than 70 characters
-      $msg = wordwrap($msg,70);
-      $return_email = $_REQUEST['email'];
+    // the message
+    $msg = $_REQUEST['message'];
+    // use wordwrap() if lines are longer than 70 characters
+    $msg = wordwrap($msg,70);
+    $from = $_REQUEST['email'];
 
-      $phone = "";
-      if (isset($_REQUEST["phone"]) && !empty($_REQUEST["phone"])) {
-        $phone = $_REQUEST['phone'];
-      }
+    $phone = "";
+    if (isset($_REQUEST["phone"]) && !empty($_REQUEST["phone"])) {
+      $phone = $_REQUEST['phone'];
+    }
 
-      $name = "";
-      if (isset($_REQUEST["name"]) && !empty($_REQUEST["name"])) {
-        $name = $_REQUEST['name'];
-      }
+    $name = "";
+    if (isset($_REQUEST["name"]) && !empty($_REQUEST["name"])) {
+      $name = $_REQUEST['name'];
+    }
 
-      // send email
-      mail(
-        "stevenr4@stevenr4.com",
-        "CONTACT FROM stevenr4.com",
-        "Sender: (" . $return_email . ")\n" .
-        "Phone: (" . $phone . ")\n" .
-        "Name: (" . $name . ")\n" .
-        "Message: " . $msg
-      );
-      mail(
-        "stevenr180@gmail.com",
-        "CONTACT FROM stevenr4.com",
-        "Sender: (" . $return_email . ")\n" .
-        "Phone: (" . $phone . ")\n" .
-        "Name: (" . $name . ")\n" .
-        "Message: " . $msg
-      );
+    // Pear Mail Library
+    require_once "Mail.php";
+
+    $to = 'stevenr180@gmail.com';
+    $subject = 'CONTACT! ('.$name.')';
+    $body = "Sender: (" . $return_email . ")\n" .
+            "Phone: (" . $phone . ")\n" .
+            "Name: (" . $name . ")\n" .
+            "Message: " . $msg;
+
+    $headers = array(
+        'From' => $from,
+        'To' => $to,
+        'Subject' => $subject
+    );
+
+    $smtp = Mail::factory('smtp', array(
+            'host' => 'ssl://smtp.gmail.com',
+            'port' => '465',
+            'auth' => true,
+            'username' => 'johndoe@gmail.com',
+            'password' => getenv('GMAIL_PASS')
+        ));
+
+    $mail = $smtp->send($to, $headers, $body);
+
+    if (PEAR::isError($mail)) {
+        echo('<p>' . $mail->getMessage() . '</p>');
+    } else {
+        echo('<p>Message successfully sent!</p>');
+    }
     $output['success'] = 'true';
   } else {
     $output['success'] = 'false';
