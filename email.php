20a3a837d1910423d28d99891b06e1185928bd1c
stevenr4 <stevenr41@hotmail.com>
2016-02-04 15:19:34 -0800

    IT WORKS

--- email.php
+++ email.php
@@ -3,20 +3,23 @@
   if ((isset($_REQUEST["email"]) && !empty($_REQUEST["email"])) &&
       (isset($_REQUEST["message"]) && !empty($_REQUEST["message"]))) {
 
-    // the message
+    $requester = $_REQUEST['email'];
     $msg = $_REQUEST['message'];
     // use wordwrap() if lines are longer than 70 characters
     $msg = wordwrap($msg,70);
-    $requester = $_REQUEST['email'];
-
     $phone = "";
-    if (isset($_REQUEST["phone"]) && !empty($_REQUEST["phone"])) {
-      $phone = $_REQUEST['phone'];
-    }
-
+    if (isset($_REQUEST["phone"]) && !empty($_REQUEST["phone"])) { $phone = $_REQUEST['phone']; }
     $name = "";
-    if (isset($_REQUEST["name"]) && !empty($_REQUEST["name"])) {
-      $name = $_REQUEST['name'];
+    if (isset($_REQUEST["name"]) && !empty($_REQUEST["name"])) { $name = $_REQUEST['name']; }
+    
+
+
+    // Validate the requester email:
+    if (!filter_var($requester, FILTER_VALIDATE_EMAIL)) {
+        $output['success'] = 'false';
+        $output['reason'] = 'Email is invalid';
+        echo json_encode($output);
+        die();
     }
 
     $subject = 'CONTACT from name:(' . $name . '), email:(' . $requester . ')';
@@ -33,29 +36,33 @@
 
     // Require the swift library
     require_once 'vendor/swiftmailer/swiftmailer/lib/swift_required.php';
-
     // Create the Transport, attach to GMAIL's smtp service
     $transporter = Swift_SmtpTransport::newInstance('smtp.gmail.com', 465, 'ssl')
       ->setUsername("stevenr4@stevenr4.com")
       ->setPassword(getenv('GMAIL_PASS'));
-
     // Create the Mailer using your created Transport
     $mailer = Swift_Mailer::newInstance($transporter);
 
-    // Create a message
+    // Create the message to myself
     $message = Swift_Message::newInstance($subject)
       ->setFrom(array('info@stevenr4.com' => 'Info'))
       ->setTo(array('stevenr4@stevenr4.com', 'stevenr180@gmail.com'))
       ->setBody($body)
       ;
+    // Send the message to myself
+    $result = $mailer->send($message);
 
-    // Send the message
+    // Create a user-facing message:
+    $message = Swift_Message::newInstance("Steven Rogers (automated response)")
+      ->setFrom(array('info@stevenr4.com' => 'Info'))
+      ->setTo(array($requester))
+      ->setBody("Dear " . $name . ",\n\n\tThank you for reaching out to me.\nThe following message has been sent to my email inbox\nand I will get back to you shortly.\n\n" . $body)
+      ;
+    // Send the user-facing message
     $result = $mailer->send($message);
     $output['success'] = 'true';
   } else {
-    $output['pass'] = getenv('GMAIL_PASS');
     $output['success'] = 'false';
   }
   echo json_encode($output);
-  echo $_REQUEST;
 ?>
